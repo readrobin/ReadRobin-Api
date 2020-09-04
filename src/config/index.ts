7306f73ebc29155ae6ce40d678d27a3b4f8444f0
wormjim <gcooled29@gmail.com>
2020-09-04 11:09:04 -0400

    :outbox_tray: Export From Index File

---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+export * from 'src/config/orm.config';
+export { default as ConnectionOptions } from 'src/config/orm.config';
---
+++ src/core/index.ts
@@ -0,0 +1,2 @@
+export * from 'src/core/resolvers';
+export { default as resolvers } from 'src/core/resolvers';
